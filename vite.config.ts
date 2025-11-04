470c687e1e5687ca959110681d07c34aae5cdf0e
cauatardetti <cauabtvendrame@gmail.com>
2025-11-04 11:13:12 -0300

    deploy com github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Base dinâmica para GitHub Pages (BASE_PATH no build; '/' no dev)
-const base = process.env.BASE_PATH || '/'
+// Base dinï¿½mica para GitHub Pages (BASE_PATH no build; '/' no dev)
 
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/ac_atualizar_dashboard/',
 })
